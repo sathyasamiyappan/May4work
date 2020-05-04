36ca7a980dcffdd5142fac676ed52ca0543c5cb2
sathyasamiyappan <sathya133.kec@gmail.com>
2020-05-04 20:39:09 +0530

    bug fix missed 2 in the password if condition

--- ImageHoster/src/main/java/ImageHoster/controller/UserController.java
+++ ImageHoster/src/main/java/ImageHoster/controller/UserController.java
@@ -97,7 +97,7 @@ public class UserController {
                         || password.contains("X") || password.contains("Z")
                 ) {
                     if (password.contains("0") || password.contains("1")
-                            || password.contains("3") || password.contains("3")
+                            || password.contains("2") || password.contains("3")
                             || password.contains("4") || password.contains("5")
                             || password.contains("6") || password.contains("7")
                             || password.contains("8") || password.contains("9")
